51c356383e7b8f3a31db7e55a2e5834e2dd245e8
Robert <robbystoned@gmail.com>
2020-10-28 10:26:43 +0100

    Initial Commit

---
+++ ANPRScanner/ANPRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANPRScannerApp.swift
+//  ANPRScanner
+//
+//  Created by Angelique van der Steen on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ANPRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
